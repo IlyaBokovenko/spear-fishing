480d4f10d7590e837269e3bf2af716faec1fa290
Ilya Romanov <ilya@ilya.local>
2009-10-15 17:23:08 +0400

    #1116 sharks must not be harpooned

--- Assets/_scripts/fish/AI/PredatorFishAI.cs
+++ Assets/_scripts/fish/AI/PredatorFishAI.cs
@@ -3,8 +3,5 @@ using System.Collections;
 
 public class PredatorFishAI : FishAI {
     public override void OnHit(Spear spear){
-        Transform myAnimatedBody = transform.Find("animatable");
-		if(myAnimatedBody != null)
-			spear.DecorativeStickOn(myAnimatedBody.gameObject);
     }
 }
